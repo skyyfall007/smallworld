c509b931c161dcd180dcf1a0ffe90d3c5da76dd7
ankithsubramanya <ankiths2@illinois.edu>
2017-07-28 08:25:11 -0500

    new file:   .idea/watcherTasks.xml 	modified:   .idea/workspace.xml 	new file:   Procfile.txt 	modified:   app.js 	modified:   bin/www 	modified:   node_modules/body-parser/package.json 	modified:   node_modules/express/package.json
    	modified:   package-lock.json
    	modified:   package.json
    	new file:   routes/cities.js
    	new file:   routes/countries.js
    	new file:   routes/flights.js
    	new file:   routes/hotels.js
    	new file:   routes/restaurants.js
    	new file:   routes/trips.js
    	new file:   server.js

--- app.js
+++ app.js
@@ -7,6 +7,12 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var cities = require('./routes/cities');
+var flights = require('./routes/flights');
+var countries = require('./routes/countries');
+var hotels = require('./routes/hotels');
+var restaurants = require('./routes/restaurants');
+var trips = require('./routes/trips');
 
 var app = express();
 
@@ -23,7 +29,13 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
+app.use('/api/users', users);
+app.use('/api/cities', cities);
+app.use('/api/flights', flights);
+app.use('/api/countries', countries);
+app.use('/api/hotels', hotels);
+app.use('/api/restaurants', restaurants);
+app.use('/api/trips', trips);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
