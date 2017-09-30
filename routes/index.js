edf0ab008145ba864e843b30d648a6672e45deb9
ankithsubramanya <ankiths2@illinois.edu>
2017-09-30 16:43:33 -0500

    changed index.jade to index.html

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render("../views/index.jade");
+  res.render("../views/index.html");
 });
 
 module.exports = router;
