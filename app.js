108919bb38e63be9a29ad4f53a03af79dbec457a
ankithsubramanya <ankiths2@illinois.edu>
2017-09-30 15:26:41 -0500

    With front end

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 
 // uncomment after placing your favicon in /public
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -42,7 +42,7 @@ app.use('/api/countries', countries);
 app.use('/api/hotels', hotels);
 app.use('/api/restaurants', restaurants);
 app.use('/api/trips', trips);
-app.use('/api/');
+app.use('/api/userTrips', userTrips);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
