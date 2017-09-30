89acdbffa30ae84005914e265a2684ca3abb1506
ankithsubramanya <ankiths2@illinois.edu>
2017-09-30 16:21:06 -0500

    With front end

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.sendFile("../views/index.html");
+  res.render("../views/index.jade");
 });
 
 module.exports = router;
